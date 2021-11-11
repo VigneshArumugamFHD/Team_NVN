215de8821cff925405a62dc6a9aa96534fb1ab78
nmktuncbilek <93388611+nmktuncbilek@users.noreply.github.com>
2021-11-11 22:36:19 +0100

    exercise_02

--- dps_task1_teamNVNO.c
+++ dps_task1_teamNVNO.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <omp.h>
 
 pthread_mutex_t count_mutex     = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t condition_mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -9,19 +10,22 @@ pthread_cond_t  condition_cond  = PTHREAD_COND_INITIALIZER;
 void *functionCount1();
 void *functionCount2();
 int  count = 0;
-#define COUNT_DONE  99
-#define COUNT_HALT1  3
-#define COUNT_HALT2  6
+#define COUNT_DONE  93
+#define COUNT_HALT1  2
+#define COUNT_HALT2  4
 
 int main()
 {
+   #pragma omp parallel num_threads(4)
+   {
+
    pthread_t thread1, thread2;
 
    pthread_create( &thread1, NULL, &functionCount1, NULL);
    pthread_create( &thread2, NULL, &functionCount2, NULL);
    pthread_join( thread1, NULL);
    pthread_join( thread2, NULL);
-
+   }
    exit(0);
 }
 
@@ -38,11 +42,11 @@ void *functionCount1()
 
       pthread_mutex_lock( &count_mutex );
       count++;
-      printf("Total Vistors inside the Garden[West Gate]: %d\n",count);
+      printf("Total Visitors inside the Garden[West Gate]: %d\n",count);
       pthread_mutex_unlock( &count_mutex );
 
       if(count >= COUNT_DONE) return(NULL);
-    }
+   }
 }
 
 void *functionCount2()
@@ -58,7 +62,7 @@ void *functionCount2()
 
        pthread_mutex_lock( &count_mutex );
        count++;
-       printf("Total Vistors inside the Garden[East Gate]: %d\n",count);
+       printf("Total Visitors inside the Garden[East Gate]: %d\n",count);
        pthread_mutex_unlock( &count_mutex );
 
        if(count >= COUNT_DONE) return(NULL);
