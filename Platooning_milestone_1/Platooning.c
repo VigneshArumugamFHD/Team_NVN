b9baf4de6169f4a73fa7b356a536159c6c9f56f0
Team_NVN <93388679+VigneshArumugamFHD@users.noreply.github.com>
2021-11-12 08:38:51 +0100

    Code_merge

--- Platooning_milestone_1/Platooning.c
+++ Platooning_milestone_1/Platooning.c
@@ -2,22 +2,73 @@
 #include<conio.h>
 #include<pthread.h>
 
-
-
-void platooning_Initialization();
+int *initializePlatoon(int n,int d, int x1, int y1);
 
 
 void main()
 {
 
    /*Starting of PLatoon*/
-       /* - */
-	   
-	static int NumofTrucks;
+       /* - */	
+	int nTrucks;
+	int dis;
+	int x1, y1;
+	int rVelocity;
+	int accel;
+	int choice;
+	int nSteps = 5;
+	int j = 0;
+	bool platooning;
+	
+	printf("Please enter number of trucks: ");
+	scanf("%d", &nTrucks);
+	printf("Please enter number of distance: ");
+	scanf("%d", &dis);
+	printf("Please enter start of x coordinate: ");
+	scanf("%d", &x1);
+	printf("Please enter start of y coordinate: ");
+	scanf("%d", &y1);
 	
 	
+	int * arr;
+	
+	
+	int arrx[nTrucks];
+	int arry[nTrucks];
+	arrx[0] = x1;
+	arry[0] = y1;
+	for(int i = 1; i < nTrucks; i++){
+		arrx[i] = arrx[i-1] + dis;
+		arry[i] = arry[i-1] + dis;
+	}
+	
+	while(true){
+		for(int k = j*nSteps; k < nSteps*(j+1); k++){
+			for(int i = 0; i < nTrucks; i++){
+				printf("x%d:%d-y%d:%d\n",i+1,arrx[i] + k*dis,i+1,arry[i] + k*dis);
+			}
+			printf("\n");
+			
+		}
+		
+		printf("run or stop ");
+		printf("\n");
+		scanf("%d",&platooning);
+		//printf("%d",platooning);
+		switch(platooning){
+			case 0:
+				goto EndWhile;
+				break;
+		
+		}
+		
+		
+		j++;
+	}
+	EndWhile: ;
+	return 0;
     
-    platooning_Initialization();
+    initializePlatoon();
 
     Update_Leader_coordinates();
 
@@ -29,32 +80,14 @@ void main()
 
     while()
     {
-        
+        /*Error handling*/
+		/*Checking for coupling & de-coupling*/
     }
 
-
-
-   /*Checking for coupling & de-coupling*/
-
-
-
-
-
-   /*Eroor*/
-
-
-
 }
 
-void platooning_Initialization()
-{
-	
-	
-	static int DesiredDistance;
-	
-	
+int *initilizePlatoon(int n,int d, int x1, int y1){
 	
 	
 	
 }
-
