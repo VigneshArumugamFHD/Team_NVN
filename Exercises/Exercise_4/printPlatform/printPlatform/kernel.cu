864c23fc81b68f9e5e6d7dc884cc1d6499c46ab5
nmktuncbilek <93388611+nmktuncbilek@users.noreply.github.com>
2021-11-23 02:16:54 +0100

    Update kernel

--- Exercises/Exercise_4/printPlatform/printPlatform/kernel.cu
+++ Exercises/Exercise_4/printPlatform/printPlatform/kernel.cu
@@ -1,24 +1,50 @@
-﻿#include<stdio.h>
-#include<CL/cl.h>
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <OpenCL/cl.h>
 
 
 
-int main() 
-{
+int main() {
 
-            for (const cl::sycl::platform& platform : cl::sycl::platform::get_platforms()) 
-            {
+    int i, j;
+    char* info;
+    size_t infoSize;
+    cl_uint platformCount;
+    cl_platform_id* platforms;
 
-            std::cout << " ##Platform## " << std::endl;
-            std::cout << " Name   : "
-                << platform.get_info< cl::sycl::info::platform::name >() << std::endl;
-            std::cout << " Vendor : "
-                << platform.get_info< cl::sycl::info::platform::vendor >() << std::endl;
-            std::cout << " Version: "
-                << platform.get_info< cl::sycl::info::platform::version >() << std::endl;
 
-     
-            }
+    const char* attributeNames[5] = { "Name", "Vendor", "Version", "Profile", "Extensions" };
 
-            return 0;
+    const cl_platform_info attributeTypes[5] = { CL_PLATFORM_NAME, CL_PLATFORM_VENDOR, CL_PLATFORM_VERSION, CL_PLATFORM_PROFILE, CL_PLATFORM_EXTENSIONS };
+    const int attributeCount = sizeof(attributeNames) / sizeof(char*);
+
+    clGetPlatformIDs(5, NULL, &platformCount);
+
+    platforms = (cl_platform_id*)malloc(sizeof(cl_platform_id) * platformCount);
+    clGetPlatformIDs(platformCount, platforms, NULL);
+
+
+    for (i = 0; i < platformCount; i++) {
+
+        for (j = 0; j < attributeCount; j++) {
+
+            clGetPlatformInfo(platforms[i], attributeTypes[j], 0, NULL, &infoSize);
+            info = (char*)malloc(infoSize);
+
+            clGetPlatformInfo(platforms[i], attributeTypes[j], infoSize, info, NULL);
+
+            printf("%s: %s\n", i + 1, j + 1, attributeNames[j], info);
+
+            free(info);
+
+        }
+
+        printf("\n");
+
+    }
+
+    free(platforms);
+
+    return 0;
+
 }
