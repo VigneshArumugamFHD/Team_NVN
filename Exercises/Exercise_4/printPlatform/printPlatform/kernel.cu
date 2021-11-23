3cd7a82609a7726927b5b37de4bdc27cb5e7a603
nmktuncbilek <93388611+nmktuncbilek@users.noreply.github.com>
2021-11-23 01:07:02 +0100

    printing platform

---
+++ Exercises/Exercise_4/printPlatform/printPlatform/kernel.cu
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+#include<CL/cl.h>
+
+
+
+int main() 
+{
+
+            for (const cl::sycl::platform& platform : cl::sycl::platform::get_platforms()) 
+            {
+
+            std::cout << " ##Platform## " << std::endl;
+            std::cout << " Name   : "
+                << platform.get_info< cl::sycl::info::platform::name >() << std::endl;
+            std::cout << " Vendor : "
+                << platform.get_info< cl::sycl::info::platform::vendor >() << std::endl;
+            std::cout << " Version: "
+                << platform.get_info< cl::sycl::info::platform::version >() << std::endl;
+
+     
+                return 0;
+            }
+}
