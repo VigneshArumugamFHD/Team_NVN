b18875b9050aa0b34c8fcb37d8ea1951f56432c0
Team_NVN <93388679+VigneshArumugamFHD@users.noreply.github.com>
2021-11-23 07:57:38 +0100

    code_Update

--- Exercises/Exercise_3/Garden_Socket/main.c
+++ Exercises/Exercise_3/Garden_Socket/main.c
@@ -3,11 +3,14 @@
 */
 
 #include<stdio.h>
+
+#define _WIN32_WINNT 0x6000
 #include<winsock2.h>
+#include<ws2tcpip.h>
 
 
-#define NI_MAXHOST 1025
-#define NI_MAXSERV  32
+//#define NI_MAXHOST 1025
+//#define NI_MAXSERV  32
 //#pragma comment(lib,"ws2_32.lib") //Winsock Library
 
 int main(int argc , char *argv[])
@@ -31,13 +34,13 @@ int main(int argc , char *argv[])
 
 
 	//Create a socket
-	if((listening = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
+	if((listening = socket(AF_INET , SOCK_STREAM , 0 )) == -1)
 	{
 		printf("Could not create socket : %d" , WSAGetLastError());
 	}
 	else
     {
-        printf("Socket created.\n");
+        printf("Socket created, listening = %d .\n",listening);
 	}
 
 	//server.sin_addr.s_addr = inet_addr("192.168.56.1");
@@ -47,18 +50,51 @@ int main(int argc , char *argv[])
 
 	bind(listening, (struct sockaddr *)&server ,sizeof(server));
 
-	listen(listening, SOMAXCONN);
+	printf("Address %u and size %d\n",(struct sockaddr *)&server,sizeof(server));
+
+	printf("Status of listening %d\n",listen(listening, SOMAXCONN));
 
 
 	struct sockaddr_in client;
 
-	SOCKET clientSocket = accept(listening,(struct sockaddr*)&client, sizeof(client));
+	client.sin_family = AF_INET;
+    client.sin_port = htons(8000);
 
-	char host[NI_MAXHOST];
-	char service[NI_MAXSERV];
+    int client_size = sizeof(client);
 
-	ZeroMemory(host, NI_MAXHOST);
+	SOCKET clientSocket = socket(AF_INET , SOCK_STREAM , 0 );
+
+    SOCKET new_server_soc_connect = accept(listening,(struct sockaddr*)&client, (socklen_t*)&client_size);
+
+	//Connect to remote server
+	if (connect(clientSocket , (struct sockaddr *)&client , sizeof(client)) < 0)
+	{
+		printf("connect error\n");
+	}
+	else
+    {
+        printf("Connected\n");
+    }
+
+
+
+
+
+
+	printf("Client socket %d\n",clientSocket);
+
+    char host[NI_MAXHOST];
+    char service[NI_MAXSERV];
+
+    ZeroMemory(host, NI_MAXHOST);
 	ZeroMemory(service, NI_MAXSERV);
+
+
+
+
+
+
+	getnameinfo((struct sockaddr*)&client, sizeof(client),host, NI_MAXHOST, service, NI_MAXSERV, 0);
 
 	if(getnameinfo((struct sockaddr*)&client, sizeof(client),host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
     {
@@ -67,29 +103,26 @@ int main(int argc , char *argv[])
     }
     else
     {
-        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-        printf("%s connected on port %s",host, ntohs(client.sin_port));
+      //  inet_ntoa()
+       // inet_ntop()
+      //  inet_ntoa(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+        printf("%s connected on port %s");
     }
+
+
 
     closesocket(listening);
 
-	//Connect to remote server
-	if (connect(listening , (struct sockaddr *)&server , sizeof(server)) < 0)
-	{
-		puts("connect error");
-		return 1;
-	}
 
-	puts("Connected");
 
 	//Send some data
-	message = "GET / HTTP/1.1\r\n\r\n";
-	if( send(listening , message , strlen(message) , 0) < 0)
-	{
-		puts("Send failed");
-		return 1;
-	}
-	puts("Data Send\n");
+	//message = "GET / HTTP/1.1\r\n\r\n";
+	//if( send(listening , message , strlen(message) , 0) < 0)
+///	{
+//		puts("Send failed");
+//		return 1;
+//	}
+//	puts("Data Send\n");
 
-	return 0;
+	//return 0;
 }
